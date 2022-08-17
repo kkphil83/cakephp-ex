46c249fa238f7ce6f10b877d5485021d9cb65937
kkphil83 <48905920+kkphil83@users.noreply.github.com>
2022-08-17 13:53:28 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+/**<?php
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -11,6 +11,33 @@
  * @link          https://cakephp.org CakePHP(tm) Project
  * @since         0.10.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
- */
-
+ *
+ *
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+*/
+<?php
+ 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+$url = "https://el-dev-app-app-dev.apps.ocp4.*****.com/";
+$data = array(
+    'empty' => array()
+);
+ 
+$options = array(
+  'http' => array(
+    'method'  => 'POST',
+    'content' => json_encode( $data ),
+    'header'=>  "Content-Type: application/json\r\n"
+    )
+);
+ 
+$context  = stream_context_create( $options );
+$result = file_get_contents( $url, false, $context );
+$response = json_decode( $result );
+ 
+echo "Pipeline Start\n";
+ 
+?>
