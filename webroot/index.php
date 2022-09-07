d320a8f98fadf8ad4cbaaa4fc78c3b127db19caf
kkphil83 <48905920+kkphil83@users.noreply.github.com>
2022-09-07 10:50:58 +0900

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
  
-$url = "https://el-petclinic-cicd-slack.apps.cluster-wm5hc.wm5hc.sandbox1320.opentlc.com/";
+$url = "https://el-petclinic-cicd-slack.apps.cluster-9d5n5.9d5n5.sandbox834.opentlc.com/";
 $data = array(
     'empty' => array()
 );
